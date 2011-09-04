0c45be05cb93d1e1a0c33e0ea2080dd343920e00
Erich Schroeter <erichschroeter@gmail.com>
2011-09-03 20:49:52 -0500

    Removed classes that depended on JDAF.

--- src/org/conversionsvg/gui/preferences/ISettingsPanel.java
+++
@@ -1,8 +0,0 @@
-package org.conversionsvg.gui.preferences;
-
-import java.util.Properties;
-
-public interface ISettingsPanel {
-	Properties getProperties();
-	void setProperties(Properties props);
-}
